d45eb23dc5cc506524bde36867edba58aa792474
Drew Harris <drewbharris@gmail.com>
2013-02-11 15:28:49 -0800

    added state fetch api->ws route

--- index.js
+++ index.js
@@ -28,6 +28,24 @@ app.post('/api/v1/:client/:port', function(req, res){
 app.get('/api/v1/:client/:port', function(req, res){
     console.log('got a get state request for port ' + req.params.port + 
         ' for client ' + req.params.client);
+
+    // check to see if the requested client is connected
+    if (!connectedClients.hasOwnProperty(req.params.client)){
+        return res.send('this client is offline');
+    }
+    
+    var ws = connectedClients[req.params.client];
+
+    // send a request through the websocket to query the state of the requested port
+    ws.emit('get', {
+        'port': req.params.port
+    });
+
+    // wait for a response
+    ws.on('response', function(data){
+        res.send('the state of port ' + data.port + ' is ' + data.state);
+    });
+
 });
 
 // WebSocket routes
@@ -36,6 +54,10 @@ io.sockets.on('connection', function (socket) {
     socket.on('client-id', function(clientId){
         console.log('got connection from ' + clientId);
         connectedClients[clientId] = socket;
+        socket.on('disconnect', function(){
+            console.log(clientId + ' disconnected');
+            delete connectedClients[clientId];
+        });
     });
 });
 
