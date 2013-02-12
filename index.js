6c5117c30a49a0ce0d4d8ebf6686d2440e4ff4c2
Drew Harris <drewbharris@gmail.com>
2013-02-12 08:46:19 -0800

    s/client/remote

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
 var app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    connectedClients = {};
+    connectedDevices = {};
 
 // set up the Express static file serving
 // @todo replace with nginx for this stuff
@@ -20,21 +20,21 @@ app.get('/', function(req, res){
 
 // API routes
 
-app.post('/api/v1/:client/:port', function(req, res){
+app.post('/api/v1/:device/:port', function(req, res){
     console.log('got a ' + req.body.state + ' state request for port ' + req.params.port + 
-        ' for client ' + req.params.client);
+        ' for device ' + req.params.device);
 });
 
-app.get('/api/v1/:client/:port', function(req, res){
+app.get('/api/v1/:device/:port', function(req, res){
     console.log('got a get state request for port ' + req.params.port + 
-        ' for client ' + req.params.client);
+        ' for device ' + req.params.device);
 
-    // check to see if the requested client is connected
-    if (!connectedClients.hasOwnProperty(req.params.client)){
-        return res.send('this client is offline');
+    // check to see if the requested device is connected
+    if (!connectedDevices.hasOwnProperty(req.params.device)){
+        return res.send('this device is offline');
     }
     
-    var ws = connectedClients[req.params.client];
+    var ws = connectedDevices[req.params.device];
 
     // send a request through the websocket to query the state of the requested port
     ws.emit('get', {
@@ -51,12 +51,12 @@ app.get('/api/v1/:client/:port', function(req, res){
 // WebSocket routes
 io.sockets.on('connection', function (socket) {
     console.log('connection');
-    socket.on('client-id', function(clientId){
-        console.log('got connection from ' + clientId);
-        connectedClients[clientId] = socket;
+    socket.on('device-id', function(deviceId){
+        console.log('got connection from ' + deviceId);
+        connectedDevices[deviceId] = socket;
         socket.on('disconnect', function(){
-            console.log(clientId + ' disconnected');
-            delete connectedClients[clientId];
+            console.log(deviceId + ' disconnected');
+            delete connectedDevices[deviceId];
         });
     });
 });
