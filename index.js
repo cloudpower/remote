2f85cfd05a3525de93dcedea614986ce6a241c05
Nathan Willson <willson.b.nathan@gmail.com>
2013-03-11 13:57:20 -0700

    latest client. Still needs custom dates

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var express = require('express'),
     guid = require('guid'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
-    p = require('ua-parser');
+    p = require('ua-parser'),
+    Db = require('./lib/db');
 
 var app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     connectedDevices = {},
-    db = require('./lib/db').create('remote.db');
+    db = Db.create('tcp://localhost');
 
 // set up the Express static file serving
 // @todo replace with nginx for this stuff
@@ -20,18 +21,18 @@ app.use(express.bodyParser());
 app.use(passport.initialize());
 app.use(app.router);
 
-// set up the user authentication
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        db.authenticate(username, password).then(function() {
-            return done(null, {
-                'username': username
-            });
-        }, function(err){
-            return done(err);
-        });
-    }
-));
+// // set up the user authentication
+// passport.use(new LocalStrategy(
+//     function(username, password, done) {
+//         db.authenticate(username, password).then(function() {
+//             return done(null, {
+//                 'username': username
+//             });
+//         }, function(err){
+//             return done(err);
+//         });
+//     }
+// ));
 
 
 // these should be templated/bootstrapped to prevent excessive AJAXing
@@ -115,6 +116,51 @@ app.get('/api/v1/device/:device/:outlet', function(req, res){
 
 });
 
+// Request data from server
+app.get('/api/v1/user/:username/usage/:startDate/:endDate', function(req, res){
+    console.log('got a get state request for user ' + req.params.username);
+
+    var startDate = new Date(parseInt(req.params.startDate, 10));
+    var endDate = new Date(parseInt(req.params.endDate, 10));
+
+    console.log(startDate);
+    console.log(endDate);
+
+    db.selectData(req.params.username, startDate, endDate).then(function(result){
+        console.log(result);
+        res.send(result);
+    }, function(err){
+        console.log(err);
+        res.send(err);
+    });
+});
+
+// Request DEVICES from server
+app.get('/api/v1/user/:username/devices', function(req, res){
+    console.log('got a get state request for user ' + req.params.username);
+
+    db.selectDevices(req.params.username).then(function(result){
+        console.log(result);
+        res.send(result);
+    }, function(err){
+        console.log(err);
+        res.send(err);
+    });
+});
+
+// Request creation date
+app.get('/api/v1/user/:username/creation', function(req, res){
+    console.log('got a get state request for user ' + req.params.username);
+
+    db.selectCreationDate(req.params.username).then(function(result){
+        console.log(result);
+        res.send(result);
+    }, function(err){
+        console.log(err);
+        res.send(err);
+    });
+});
+
 // id generator
 app.get('/api/v1/id', function(req, res){
     var id = guid.create().value;
@@ -123,32 +169,32 @@ app.get('/api/v1/id', function(req, res){
     });
 });
 
-// USER ROUTES
-app.get('/api/v1/authenticate', passport.authenticate('local', { session: false }), function(req, res){
-    res.send('hello');
-});
+// // USER ROUTES
+// app.get('/api/v1/authenticate', passport.authenticate('local', { session: false }), function(req, res){
+//     res.send('hello');
+// });
 
-app.get('/api/v1/user/:username', function(req, res){
-    db.getUser(req.params.username).then(function(user){
-        // success
-        res.send(user);
-    }, function(){
-        // failure
-        res.send(404);
-    });
-});
+// app.get('/api/v1/user/:username', function(req, res){
+//     db.getUser(req.params.username).then(function(user){
+//         // success
+//         res.send(user);
+//     }, function(){
+//         // failure
+//         res.send(404);
+//     });
+// });
 
-app.post('/api/v1/user', function(req, res){
-    db.insertUser(req.body.username, req.body.password).then(function(){
-        //success
-        res.send({
-            'username': req.body.username
-        });
-    }, function(){
-        // failure
-        res.send(500);
-    });
-});
+// app.post('/api/v1/user', function(req, res){
+//     db.insertUser(req.body.username, req.body.password).then(function(){
+//         //success
+//         res.send({
+//             'username': req.body.username
+//         });
+//     }, function(){
+//         // failure
+//         res.send(500);
+//     });
+// });
 
 // WebSocket routes
 io.sockets.on('connection', function (socket) {
@@ -159,6 +205,9 @@ io.sockets.on('connection', function (socket) {
         socket.on('disconnect', function(){
             console.log(deviceId + ' disconnected');
             delete connectedDevices[deviceId];
+        });
+        socket.on('usage_data', function(data){
+            db.insertUsageData(data);
         });
     });
 });
